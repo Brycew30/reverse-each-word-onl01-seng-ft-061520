2bbf9d0395b3797752ac48a95cdf3bccddfe6a82
Bryce Whittington <brycew30@gmail.com>
2020-06-17 23:56:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
  array = sentence.split(" ")
  reversed_array = []
- array.each do |sentence|
+ array.each do|sentence|
    reversed_array << sentence.reverse
  end
 end
